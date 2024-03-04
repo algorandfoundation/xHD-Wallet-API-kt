6c1fdf40e78d9bed3900e2d95bec20a65bf7689c
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2024-03-04 13:31:25 +0100

    feat: fixes initialization

--- initialize.sh
+++ initialize.sh
@@ -1,8 +1,16 @@
-git submodule init && git submodule update
-gradle wrapper
+echo "Initializing..."
+echo "Cloning lazysodium-java..."
+rm -r lazysodium-java && git submodule init && git submodule update
+
+echo "Building lazysodium-java..."
 cd lazysodium-java
 ./gradlew build
+
+echo "Copying lazysodium-java to lib/libs..."
 cd ..
 mkdir -p lib/libs
 mv lazysodium-java/build/libs/* lib/libs/
+
+echo "Running tests..."
+gradle wrapper
 ./gradlew test
