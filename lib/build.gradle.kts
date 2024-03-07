8c35dc7675d719d0355d7ac85fe9f4f1b4225789
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2024-03-07 23:23:47 +0100

    feat: adds Algosdk-related tests that can be ignored in CI/CD

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -58,8 +58,16 @@ dependencies {
 // Apply a specific Java toolchain to ease working on different environments.
 java { toolchain { languageVersion.set(JavaLanguageVersion.of(21)) } }
 
+// Run ./gradlew test to execute all the test
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
     testLogging.showStandardStreams = true
+}
+
+// Run ./gradlew testNoAlgoLocalnet to run tests that do not require an Algorand network
+tasks.register<Test>("testNoAlgoLocalnet") {
+    useJUnitPlatform {
+        exclude("**/AlgoLocalnetTests.kt") //
+    }
 }
